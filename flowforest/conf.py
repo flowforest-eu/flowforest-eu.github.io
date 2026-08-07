d61f7fc3ac6652b0c55c9b7d24c55fb901d31ac7
Rauni Lillemets <rauni.lillemets@gmail.com>
2026-08-07 10:50:40 +0300

    conf fix

--- flowforest/conf.py
+++ flowforest/conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "FlowForest"  # (translatable)
 BLOG_TITLE = "FlowForest"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "https://flowforest.eu/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
